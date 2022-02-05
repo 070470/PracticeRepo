03918c0d7fc337ff95ae71c7369ba233fda15982
Reetta Himanka <reetta.himanka@gmail.com>
2022-02-06 00:58:08 +0200

    Modified file locations and added coverage file

---
+++ DSAproj/DSAproj/test.py
@@ -0,0 +1,12 @@
+import unittest
+from calculator import Clause
+
+class Test(unittest.TestCase):
+    def testSplit(self):
+        example = Clause("1+6-3")
+        example.split()
+        self.assertTrue(isinstance(example, list),
+                            "Expected list instead of string")
+        
+if __name__ == '__main__':
+    unittest.main()
--- DSAproj/test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class Test(unittest.TestCase):
-    def testSplit(self):
-        example = Clause("1+6-3")
-        
-
-if __name__ == '__main__':
-    unittest.main()
